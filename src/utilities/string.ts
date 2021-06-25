432847aecf3c00fb05bb6aa688850d7e495f7ea9
amatotsuji <amatoten@gmail.com>
2021-06-25 20:09:15 +0900

    :+1: Added utilities functions

--- src/utilities/string.ts
+++ src/utilities/string.ts
@@ -1,2 +1,5 @@
 export const trimPrefix = (s: string, prefix: string): string =>
   s.slice(s.indexOf(prefix) + prefix.length);
+
+export const replaceAll = (s: string, target: string, subStr: string): string =>
+  s.split(target).join(subStr);
