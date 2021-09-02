69742a0026464606026e90cd3964c2cfb9a8eacc
lakesizebyter <docowj@gmail.com>
2021-09-02 18:27:51 +0900

    change design to designType

--- src/drivers/flashDriver.ts
+++ src/drivers/flashDriver.ts
@@ -13,7 +13,7 @@ export interface FlashMessageRequest {
   iconSrc?: string;
   linkTo?: string;
   pathName?: string;
-  design?: "plane" | "popup";
+  designType?: "plane" | "popup";
 }
 
 export interface FlashSource {
