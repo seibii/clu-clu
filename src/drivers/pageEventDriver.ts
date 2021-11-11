3367d561980aeeb47cfe40f39c20b9664399b0dc
lakesizebyter <docowj@gmail.com>
2021-11-11 14:29:08 +0900

    add pageEventDriver

---
+++ src/drivers/pageEventDriver.ts
@@ -0,0 +1,29 @@
+import { Stream } from "xstream";
+
+export interface PageEvent {
+  type: "focus"| "blur" | "exit";
+}
+
+export interface PageEventSource {
+  event$: Stream<PageEvent>;
+}
+
+export const makePageEventDriver = (): PageEventSource => {
+  const sources: PageEventSource = {
+    event$: Stream.create()
+  };
+
+  window.addEventListener("focus", () => {
+    sources.event$.shamefullySendNext({ type: "focus" })
+  });
+
+  window.addEventListener("blur", () => {
+    sources.event$.shamefullySendNext({ type: "blur" })
+  });
+
+  window.addEventListener('beforeunload', () => {
+    sources.event$.shamefullySendNext({ type: "exit" })
+  });
+
+  return sources;
+}
