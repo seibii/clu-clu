3195f60bb4252f1f00fbd47687e6f9510ccd6fd3
okaharuna <haruna.oka.0@gmail.com>
2022-01-12 22:23:56 +0900

    feat: add auth0-lock driver
    feat(wip): add get id token with auth0-lock
    
    chore: updated auth0-lock
    
    fix: changed addListener
    
    fix: Fixed to fail to get token
    
    refactor: delete auth0 lock client
    
    fix: changed returnUri to Required Parameters
    
    chore: add  version

---
+++ src/drivers/auth0LockDriver.ts
@@ -0,0 +1,44 @@
+import { Stream } from "xstream";
+import { Auth0Lock } from "auth0-lock";
+
+export interface Auth0LockLogoutRequest {
+  type: "logout";
+  returnUri: string;
+}
+
+export interface Auth0LockLoginRequest {
+  type: "login";
+}
+
+export type Auth0LockRequest = Auth0LockLoginRequest | Auth0LockLogoutRequest;
+interface Props {
+  domain: string;
+  clientId: string;
+  redirectUrl: string;
+  audience: string;
+  options?: Auth0LockConstructorOptions;
+}
+
+export const makeAuth0LockDriver = (
+  props: Props
+): ((stream: Stream<Auth0LockRequest>) => void) => {
+  const lock = new Auth0Lock(props.clientId, props.domain, props.options);
+  lock.on("authenticated", (_authResult) => {
+    window.location.reload();
+  });
+
+  return (stream) => {
+    stream.addListener({
+      next: (request: Auth0LockRequest) => {
+        switch (request.type) {
+          case "login":
+            lock.show();
+            break;
+          case "logout":
+            lock.logout({ returnTo: request.returnUri });
+            break;
+        }
+      },
+    });
+  };
+};
